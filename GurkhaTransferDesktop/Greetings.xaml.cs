38d76e4d0c7aa92cc3761ed01637af738513b852
Arogya <kharelarogya@hotmail.com>
2020-06-05 22:56:05 +0900

    Test UI

---
+++ GurkhaTransferDesktop/Greetings.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows;
+
+namespace GurkhaTransferDesktop
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (HelloButton.IsChecked == true)
+                MessageBox.Show("Hello");
+            else if (GoodByeButton.IsChecked == true)
+                MessageBox.Show("Goodbye");
+        }
+    }
+}
